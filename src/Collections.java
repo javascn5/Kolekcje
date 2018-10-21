f667a60b13a6e02527bf1ac989eb826c6bc98b3d
Kolachow <kolas3000@gmail.com>
2018-10-21 11:31:31 +0200

    Zad. 1 - dodanie petli for

--- src/Collections.java
+++ src/Collections.java
@@ -44,13 +44,13 @@ public class Collections {
         lista.clear();                      //usuwamy wszystkie elementy listy
         System.out.println("Rozmiar listy: " + lista.size());
 //---------------------------------------------------------------------
-//todo Zad1
+//todo Zad1 - utworzenie listy i obiektow klasy User. Nastepnie dodanie obiektow do listy people
         System.out.println("\n\nZad. 1:\n");
         List<User> people = new ArrayList<>();
 
-        User user_1 = new User("Anna", "Mita", 99);
+        User user_1 = new User("Anna", "Bajtowa", 99);
         User user_2 = new User("Michal", "Bit", 69);
-        User user_3 = new User("Cyber", "Marian", 32);
+        User user_3 = new User("Cyber", "Marian", 29);
 
         people.add(user_1);
         people.add(user_2);
@@ -60,5 +60,10 @@ public class Collections {
             System.out.println(ludziki.getName() + " " + ludziki.getLastName() + " " + ludziki.getAge());
         }
 
+        System.out.println();
+
+        for (int i = 0; i < people.size(); i ++){
+            System.out.println(people.get(i).getName() + " " + people.get(i).getLastName() + " " + people.get(i).getAge());
+        }
     }
 }
